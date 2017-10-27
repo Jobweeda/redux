f61d91d1b79c978acb41da5f24400e5726283ff7
jobweeda <jobweeda@hotmail.com>
2017-10-27 11:45:15 +0200

    adds a test for displaying the title correctly

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import Title from './components/Title';
 
+chai.use(chaiEnzyme())
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div-tag', () => {
+    expect(app).to.have.tagName('div')
+  })
+
+  it('contains a title', () => {
+    expect(app).to.have.descendants(Title)
+  })
+})
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
