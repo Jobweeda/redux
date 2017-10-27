815aeac49cc7e7fbd72ac5246fa54a9466b36331
jobweeda <jobweeda@hotmail.com>
2017-10-27 15:31:57 +0200

    adds actions for newgame and guesses

---
+++ src/actions/guess.js
@@ -0,0 +1,9 @@
+export const GUESS = 'GUESS';
+
+
+export default (guess) => {
+  return {
+  type: GUESS,
+  payload: guess
+}
+}
---
+++ src/containers/Hangman.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+
+class Hangman extends PureComponent {
+  render(){
+    return null
+  }
+}
+
+export default Hangman
